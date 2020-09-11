5bce24e2e6bedf8a5914dca57e23cd98a30ea2c4
lideming <me@yuuza.net>
2020-09-11 18:40:54 +0800

    add LyricsCopy.js

---
+++ Userscript/LyricsCopy.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         LyricsCopy
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Help you copy lyrics
+// @author       Li Deming
+// @match        https://utaten.com/lyric/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    function getUtaten() {
+        var lyricsBox = document.querySelector('.hiragana');
+        var r = '';
+        for (var x = lyricsBox.firstChild; x != null; x = x.nextSibling) {
+            if (x.nodeType == Node.TEXT_NODE) {
+                r += x.textContent.replace(/(^\s+)|(\s+$)/g, '');
+            } else if (x.nodeType == Node.ELEMENT_NODE) {
+                /** @type {HTMLElement} */
+                const element = x;
+                if (element.tagName == 'SPAN' && element.className == 'ruby') {
+                    r += '[' + element.childNodes[0].textContent + ']{' + element.childNodes[1].textContent + '}';
+                } else if (element.tagName == 'BR') {
+                    r += '\n';
+                }
+            }
+        }
+        return r;
+    }
+    window.setTimeout(() => {
+        console.log(getUtaten());
+    }, 2000);
+    window.lc = {
+        getUtaten
+    };
+})();
