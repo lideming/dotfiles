c2881174ded62500afd6bacbdf880225f3a5f7d3
lideming <me@yuuza.net>
2021-04-23 23:14:43 +0800

    update iptables

--- CodeSnippets/iptables.fish
+++ CodeSnippets/iptables.fish
@@ -3,10 +3,21 @@ exit 10
 
 
 ### Simple NAT
-set OUTERDEV enp0s31f6
-set INNERIP 10.1.0.0/24
-set IPTABLES "sudo iptables"
+set OUTERDEV eth0
+set INNERIP 10.12.34.0/24
+set IPTABLES sudo iptables
 
-$IPTABLES -t nat -A POSTROUTING -o $OUTERDEV -j MASQUERADE
-$IPTABLES -A FORWARD -i $OUTERDEV -d $INNERIP -m state --state RELATED,ESTABLISHED -j ACCEPT
-$IPTABLES -A FORWARD -s $INNERIP -o $OUTERDEV -j ACCEPT
+eval $IPTABLES -t nat -A POSTROUTING -o $OUTERDEV -j MASQUERADE
+eval $IPTABLES -A FORWARD -i $OUTERDEV -d $INNERIP -m state --state RELATED,ESTABLISHED -j ACCEPT
+eval $IPTABLES -A FORWARD -o $OUTERDEV -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+
+### Add/remove routes on Windows (Powershell)
+$gateway = "10.12.34.1"
+route add 0.0.0.0/1 $gateway
+route add 128.0.0.0/1 $gateway
+
+$gateway = "10.12.34.1"
+route delete 0.0.0.0/0 $gateway
+route delete 0.0.0.0/1 $gateway
+route delete 128.0.0.0/1 $gateway
